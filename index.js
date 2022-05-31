b6aed869f8af48b73753ba9f0918c4ca1dfca951
Obaydullah <43136881+obaydullah@users.noreply.github.com>
2022-05-31 16:07:31 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".slider-area").slick({
+    infinite: true,
+    prevArrow:
+      "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:
+      "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+  });
+});
